f11e7f3c09b12f6c09ae931a718cb1ad78a297e4
Leonardho Rante S <leonardho81@gmail.com>
2025-02-21 10:04:46 +0700

    [DS] Exploratory Data Analysis (EDA) - Pie Char 5 Top Division With Most Employee, Pie Chart Department Type Comparison, Pie Chart Race Comparison, Pie Chart Marital Status Comparison

--- hr_analytics_dataset/src/components/DataVisualization.vue
+++ hr_analytics_dataset/src/components/DataVisualization.vue
@@ -12,6 +12,14 @@
   const series_employee_type_comparison = ref([])
   const labels_gender_comparison = ref([])
   const series_gender_comparison = ref([])
+  const labels_department_type_comparison = ref([])
+  const series_department_type_comparison = ref([])
+  const labels_division_comparison = ref([])
+  const series_division_comparison = ref([])
+  const labels_race_comparison = ref([])
+  const series_race_comparison = ref([])
+  const labels_marital_comparison = ref([])
+  const series_marital_comparison = ref([])
 
   onMounted(async () => {
     const filePath = "/src/assets/Messy_HR_Dataset_Detailed.csv" 
@@ -19,6 +27,10 @@
       const data_gender_raw = await readCsv(filePath,['GenderCode'])
       const data_employee_type_raw = await readCsv(filePath,['EmployeeType'])
       const data_title_raw = await readCsv(filePath,['Title'])
+      const data_department_type_raw = await readCsv(filePath,['DepartmentType'])
+      const data_division_raw = await readCsv(filePath,['Division'])
+      const data_race_raw = await readCsv(filePath,['RaceDesc'])
+      const data_marital_raw = await readCsv(filePath,['MaritalDesc'])
 
       // Exploratory Data Analysis (EDA) - Pie Chart Gender Comparison
       const gender_comparison = countWordFrequencies(data_gender_raw['GenderCode'])
@@ -34,6 +46,26 @@
       const title_comparison = countWordFrequencies(data_title_raw['Title'],5)
       labels_title.value = Object.keys(title_comparison)
       series_title.value = Object.values(title_comparison)
+
+      // Exploratory Data Analysis (EDA) - Pie Char 5 Top Division With Most Employee
+      const division_comparison = countWordFrequencies(data_division_raw['Division'],5)
+      labels_division_comparison.value = Object.keys(division_comparison)
+      series_division_comparison.value = Object.values(division_comparison)
+
+      // Exploratory Data Analysis (EDA) - Pie Chart Department Type Comparison
+      const department_type_comparison = countWordFrequencies(data_department_type_raw['DepartmentType'])
+      labels_department_type_comparison.value = Object.keys(department_type_comparison)
+      series_department_type_comparison.value = Object.values(department_type_comparison)
+
+      // Exploratory Data Analysis (EDA) - Pie Chart Race Comparison
+      const race_comparison = countWordFrequencies(data_race_raw['RaceDesc'])
+      labels_race_comparison.value = Object.keys(race_comparison)
+      series_race_comparison.value = Object.values(race_comparison)
+
+      // Exploratory Data Analysis (EDA) - Pie Chart Marital Status Comparison
+      const marital_comparison = countWordFrequencies(data_marital_raw['MaritalDesc'])
+      labels_marital_comparison.value = Object.keys(marital_comparison)
+      series_marital_comparison.value = Object.values(marital_comparison)
     } catch (error) {
       console.error("Failed to load CSV:", error)
     }
@@ -77,6 +109,58 @@
       :labels="labels_employee_type_comparison" 
       second_title="Employee Type Comparison" 
       content="This show employee type comparison"
+    />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <!-- Exploratory Data Analysis (EDA) - Pie Char 5 Top Division With Most Employee  -->
+    <O_PieChartComponent 
+      :series="series_division_comparison" 
+      :labels="labels_division_comparison" 
+      second_title="5 Top Division With Most Employee" 
+      content="This show 5 division with most employee"
+    />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <!-- Exploratory Data Analysis (EDA) - Pie Chart Department Type Comparison  -->
+    <O_PieChartComponent 
+      :series="series_department_type_comparison" 
+      :labels="labels_department_type_comparison" 
+      second_title="Department Type Comparison" 
+      content="This show department type comparison"
+    />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <!-- Exploratory Data Analysis (EDA) - Pie Chart Race Comparison  -->
+    <O_PieChartComponent 
+      :series="series_race_comparison" 
+      :labels="labels_race_comparison" 
+      second_title="Race Comparison" 
+      content="This show race comparison"
+    />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <!-- Exploratory Data Analysis (EDA) - Pie Chart Marital Status Comparison  -->
+    <O_PieChartComponent 
+      :series="series_marital_comparison" 
+      :labels="labels_marital_comparison" 
+      second_title="Marital Status Comparison" 
+      content="This show marital status comparison"
     />
   </WelcomeItem>
 </template>
